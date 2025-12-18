b0933cc7c0aa65953ca63b1348e2281cb8e5c722
MrManoj124 <manojmanorooban@gmail.com>
2025-12-18 15:58:48 +0530

    create SplashScreen and gotoHome function for index.tsx file

--- practiceprep/app/index.tsx
+++ practiceprep/app/index.tsx
@@ -1,4 +1,41 @@
 import { Text, View } from "react-native";
+import Lottie from "lottie-react-native";
+import {useEffect, useState} from "react";
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+export default function SplashScreen()
+{
+  const [step, setSetp] = useState(0);
+  const screens = [
+    {
+      animation:require(),
+      title:"Lottie based files",
+      subtitle:"This is the first screen of the app"
+    },
+    {
+      animation:reqiuire(),
+      title:"lottie based files",
+      subtitle:"This is the second screen of the app"
+    },
+    {
+      animation:require(),
+      title:"llm cons",
+      subtitle:"This is the third screen of the app"
+    ,
+  ];
+
+  const gotoHome = () =>router.replace("/Home");
+
+  useEffect(()=>{
+    const timer=setTimeout(()=>{
+      if(step < screens.length - 1 ) setSetp(step + 1);
+      else.gotoHome();
+    },3000);
+    return ()=>clearTimeout(timer);
+  },[step]);
+
+  
+}
 
 export default function Index() {
   return (
