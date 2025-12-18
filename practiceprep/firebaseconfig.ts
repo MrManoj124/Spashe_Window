c6556b897751a96bfd5751a7d2012f81bfe0499b
MrManoj124 <manojmanorooban@gmail.com>
2025-12-19 00:40:54 +0530

    modify Login.tsx file

--- practiceprep/firebaseconfig.ts
+++ practiceprep/firebaseconfig.ts
@@ -1,30 +1,21 @@
-import firebase from '@react-native-firebase/app';
-import '@react-native-firebase/auth';
-import '@react-native-firebase/firestore';
-import '@react-native-firebase/storage';
-
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyAF3djYVjF49bD6QHsAkrwXHEVKP5dRb94",
-
   authDomain: "unimate-74.firebaseapp.com",
-
   projectId: "unimate-74",
-
-  storageBucket: "unimate-74.firebasestorage.app",
-
+  storageBucket: "unimate-74.appspot.com",
   messagingSenderId: "27455709747",
-
   appId: "1:27455709747:web:15e355452716fbc7b27946",
-
-  measurementId: "G-MT8Y453PNN"
-
 };
 
-export const app = firebase.initializeApp(firebaseConfig);
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
-export const storage = firebase.storage();
+const app = initializeApp(firebaseConfig);
 
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 
+export default app;
