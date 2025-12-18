d1819c5bdf5b4d6f82b142d137bb25b9e30112e8
MrManoj124 <manojmanorooban@gmail.com>
2025-12-18 14:38:25 +0530

    Completed the code for Home.tsx file

---
+++ practiceprep/app/Home.tsx
@@ -0,0 +1,38 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function Home(){
+    return(
+        <View style={StyleSheet.container}>
+            <Text style={StyleSheet.title}>Welcome To Practice React_Native</Text>
+            <Text style={StyleSheet.subtitle}>Open for More Details</Text>
+        </View>
+    )
+}
+
+const stytles=StyleSheet.create(
+    {
+        container:{
+            flex:1,
+            backgroundColor:"rgba(179, 134, 179, 0.47)",
+            alignItems:'Center',
+            justifyContent:'center',
+            paddingHorizontal:25,
+        },
+        title:{
+            fontSize:36,
+            fontWeight:"800",
+            marginTop:20,
+            color:"#111",
+            letterSpacing:0.5,
+        },
+        subtitle:{
+            fontSize:17,
+            color:"#6a6a6a",
+            textAlign:'center',
+            paddingHorizontal:25,
+            marginTop:10,
+            lineHeight:22,
+        }
+    }
+)
